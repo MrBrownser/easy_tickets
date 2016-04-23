59ca660b455a877e2714e5386178cc145fcf916d
Adrià Castany <adria.castany@season.es>
2016-04-23 19:10:48 +0200

    Add travelers controller

---
+++ app/controllers/travelers_controller_controller.rb
@@ -0,0 +1,10 @@
+class TravelersControllerController < ApplicationController
+  def new
+  end
+
+  def search
+  end
+
+  def show
+  end
+end
---
+++ app/helpers/travelers_controller_helper.rb
@@ -0,0 +1,2 @@
+module TravelersControllerHelper
+end
