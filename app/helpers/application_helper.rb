80c8cc1149cd5311d10bbba9bf6e43480b37ccf1
Adrià Castany <adria.castany@season.es>
2016-06-01 10:04:29 +0200

    Fix Bulma css import. Navigation menu done properly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path)
+    class_name = 'nav-item is-tab'
+    class_name += current_page?(link_path) ? ' is-active' : ''
+
+    link_to link_text, link_path, class: class_name
+  end
 end
