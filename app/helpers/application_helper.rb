038d4c036315fa131e9c0bbc210991b88da84a68
Adrià Castany <adria.castany@season.es>
2016-06-02 10:17:45 +0200

    Refine styling of home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  def nav_link(link_text, link_path)
+  def nav_link(link_text, link_path, is_enabled=true)
     class_name = 'nav-item is-tab'
     class_name += current_page?(link_path) ? ' is-active' : ''
 
-    link_to link_text, link_path, class: class_name
+    is_enabled ? (link_to link_text, link_path, class: class_name) : (link_to link_text, link_path, class: "#{class_name} is-disabled")
   end
 end
